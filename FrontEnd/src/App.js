97259ad6047409cbc0c3ecc3063c44f1f46001d4
Yoonjae <yunyun3599@gmail.com>
2021-07-09 01:00:25 +0900

    feat: SPA로 구성

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,5 +1,19 @@
+import React from "react";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import MainPage from "./pages/MainPage";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+
 function App() {
-  return <div>Main</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={MainPage} />
+        <Route exact path="/signup" component={Signup} />
+        <Route exact path="/login" component={Login} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
