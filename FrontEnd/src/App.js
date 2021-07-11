1f72af3075dccc27022eaa9e1477cb74a03e67fc
roum02 <roum02@ewhain.net>
2021-07-11 21:45:28 +0900

    fix[컴포넌트 생성 및 로그인 페이지 생성] Button, Input Component, Signup, Login, FindEmail

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -3,6 +3,8 @@ import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
+import FindEmail from "./pages/FindEmail";
+import FindPassword from "./pages/FindPassword";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <Route exact path="/" component={MainPage} />
         <Route exact path="/signup" component={Signup} />
         <Route exact path="/login" component={Login} />
+        <Route exact path="/findemail" component={FindEmail} />
+        <Route exact path="/findpassword" component={FindPassword} />
       </Switch>
     </Router>
   );
--- FrontEnd/src/pages/FindEmail.js
+++ FrontEnd/src/pages/FindEmail.js
@@ -18,33 +18,30 @@ const Items = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: space-around;
-  height: 45vh;
+  height: 35vh;
 `;
 
 const Text_box = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
-  justify-content: space-between;
-  width: 80%;
+  width: 96%;
   color: #064538;
 `;
 
-const Login = () => {
+const FindEmail = () => {
   return (
     <Wrapper>
       <Items>
         <img src={logo} style={{ marginBottom: "1rem" }}></img>
-        <Input name="E-mail" width="29rem"></Input>
-        <Input name="비밀번호" width="29rem"></Input>
-        <Button name="로그인" width="29rem" color="#56BE9C"></Button>
+        <Input name="가입된 이메일 주소를 입력해주세요" width="29rem"></Input>
         <Text_box>
-          <div>이메일 / 비밀번호 찾기</div>
-          <div>회원가입</div>
+          <div>가입된 이메일 주소로 확인 메일이 발송됩니다.</div>
         </Text_box>
+        <Button name="이메일 찾기" width="29rem" color="#56BE9C"></Button>
       </Items>
     </Wrapper>
   );
 };
 
-export default Login;
+export default FindEmail;
