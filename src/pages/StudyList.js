94ea62eba7e5e0f82933de843f16052fbf888d18
Yoonjae <yunyun3599@gmail.com>
2021-07-13 12:30:06 +0900

    스터디 리스트 창 검색바 드롭다운 구현

--- src/pages/StudyList.js
+++ src/pages/StudyList.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import Header from "../components/Header";
 import Input from "../components/Input";
 import Title from "../components/Title";
+import Image from "../components/Image";
 
 const Fix = styled.div`
   min-height: 100vh;
@@ -11,7 +12,7 @@ const Fix = styled.div`
   align-items: center;
 `;
 const Search = styled.div`
-  width: 100%;
+  width: 790%;
   display: flex;
   flex-direction: row;
   justify-content: center;
@@ -24,10 +25,14 @@ const Item = styled.div`
     margin-left: 25px;
   }
   ul {
+    width: 125px;
     display: none;
     padding-left: 0px;
     border: 1px solid #56be9c;
     border-radius: 10px;
+    z-index: 1;
+    position: absolute;
+    margin-top: 108px;
   }
   li {
     padding: 5px 10px;
@@ -43,11 +48,111 @@ const Item = styled.div`
     li {
       display: block;
       cursor: pointer;
+      background-color: white;
+      z-index: 1;
     }
   }
 `;
+const List = styled.div`
+  width: 1520px;
+  display: flex;
+  flex-direction: column;
+`;
+const Study = styled.div`
+  width: 1520px;
+  height: 92px;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+`;
 
 const StudyList = () => {
+  const [studies, setStudies] = useState([
+    {
+      studyprofile: "../assets/rectangle.png",
+      name: "스터디명",
+      state: "모집중",
+      onoff: "온라인",
+      people: 3,
+      max: 11,
+      temperature: "0",
+      owner: "김김김",
+      ownerprofie: ".../assets/circle",
+      tags: ["태그1", "태그2", "태그3", "태그4", "태그5", "태그6"],
+    },
+    {
+      studyprofile: "../assets/rectangle.png",
+      name: "스터디명",
+      state: "모집중",
+      onoff: "온라인",
+      people: 3,
+      max: 11,
+      temperature: "0",
+      owner: "김김김",
+      ownerprofie: ".../assets/circle",
+      tags: ["태그1", "태그2", "태그3", "태그4", "태그5", "태그6"],
+    },
+    {
+      studyprofile: "../assets/rectangle.png",
+      name: "스터디명",
+      state: "모집중",
+      onoff: "온라인",
+      people: 3,
+      max: 11,
+      temperature: "0",
+      owner: "김김김",
+      ownerprofie: ".../assets/circle",
+      tags: ["태그1", "태그2", "태그3", "태그4", "태그5", "태그6"],
+    },
+    {
+      studyprofile: "../assets/rectangle.png",
+      name: "스터디명",
+      state: "모집중",
+      onoff: "온라인",
+      people: 3,
+      max: 11,
+      temperature: "0",
+      owner: "김김김",
+      ownerprofie: ".../assets/circle",
+      tags: ["태그1", "태그2", "태그3", "태그4", "태그5", "태그6"],
+    },
+    {
+      studyprofile: "../assets/rectangle.png",
+      name: "스터디명",
+      state: "모집중",
+      onoff: "온라인",
+      people: 3,
+      max: 11,
+      temperature: "0",
+      owner: "김김김",
+      ownerprofie: ".../assets/circle",
+      tags: ["태그1", "태그2", "태그3", "태그4", "태그5", "태그6"],
+    },
+    {
+      studyprofile: "../assets/rectangle.png",
+      name: "스터디명",
+      state: "모집중",
+      onoff: "온라인",
+      people: 3,
+      max: 11,
+      temperature: "0",
+      owner: "김김김",
+      ownerprofie: ".../assets/circle",
+      tags: ["태그1", "태그2", "태그3", "태그4", "태그5", "태그6"],
+    },
+    {
+      studyprofile: "../assets/rectangle.png",
+      name: "스터디명",
+      state: "모집중",
+      onoff: "온라인",
+      people: 3,
+      max: 11,
+      temperature: "0",
+      owner: "김김김",
+      ownerprofie: ".../assets/circle",
+      tags: ["태그1", "태그2", "태그3", "태그4", "태그5", "태그6"],
+    },
+  ]);
   return (
     <Fix>
       <Header />
@@ -64,7 +169,7 @@ const StudyList = () => {
             <img
               alt="search"
               src={require("../assets/search.png").default}
-              style={{ width: "16px", height: "18px" }}
+              style={{ width: "16px", height: "18px", cursor: "pointer" }}
             />
           </Input>
         </Item>
@@ -177,6 +282,76 @@ const StudyList = () => {
           ></Input>
         </Item>
       </Search>
+      <List>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            marginTop: "65px",
+          }}
+        >
+          <div>
+            <Title>검색결과</Title>
+          </div>
+          <div style={{ display: "flex", flexDirection: "row" }}>
+            <div
+              style={{
+                width: "14.7px",
+                height: "25px",
+                marginRight: "72px",
+              }}
+              onClick={() => console.log("왼쪽")}
+            >
+              <Image
+                alt="left arrow"
+                src={require("../assets/leftarrow.png").default}
+                width="14.7px"
+                height="25px"
+              />
+            </div>
+            <Title color="#56BE9C">2/11</Title>
+            <div
+              style={{
+                width: "14.7px",
+                height: "25px",
+                marginLeft: "72px",
+              }}
+              onClick={() => console.log("오른쪽")}
+            >
+              <Image
+                alt="right arrow"
+                src={require("../assets/rightarrow.png").default}
+                width="14.7px"
+                height="25px"
+              />
+            </div>
+          </div>
+        </div>
+        <div
+          style={{
+            width: "1520px",
+            border: "3px solid #56BE9C",
+            marginTop: "19px",
+            background: "#56BE9C",
+          }}
+        ></div>
+        {studies.map((study, i) => {
+          if (i > 5) return false;
+          return (
+            <Study>
+              <img
+                alt="study profile"
+                src={require("../assets/rectangle.png").default}
+                style={{ width: "60px", height: "60px" }}
+              />
+              <Title size="20" weight="400">
+                {study.name}
+              </Title>
+            </Study>
+          );
+        })}
+      </List>
     </Fix>
   );
 };
