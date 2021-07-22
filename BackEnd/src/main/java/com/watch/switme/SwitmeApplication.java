ae2e6cc6cc62052118bc889d094ad246806821aa
hyojin530 <dkakfl56@gmail.com>
2021-07-22 19:40:21 +0900

    directory

---
+++ BackEnd/src/main/java/com/watch/switme/SwitmeApplication.java
@@ -0,0 +1,13 @@
+package com.watch.switme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwitmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwitmeApplication.class, args);
+	}
+
+}
---
+++ BackEnd/src/test/java/com/watch/switme/SwitmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.watch.switme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwitmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
