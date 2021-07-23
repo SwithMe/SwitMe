fd88edd30f20aaa7509fcbf02145b06b330ba3bc
Yoonjae <yunyun3599@gmail.com>
2021-07-24 01:14:20 +0900

    feat:회원가입, 랭킹, 누적 공부시간 request

--- src/_actions/actions.js
+++ src/_actions/actions.js
@@ -1,26 +1,52 @@
 import axios from "axios";
 import { USER_SERVER } from "../config";
 
-const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+const LOGIN = "LOGIN";
+const SIGNUP = "SIGNUP";
+const GET_TOTAL_TIME = "GET_TOTAL_TIME";
+const GET_RANK = "GET_RANK";
 
 export const login = (dataToSubmit) => {
   const request = axios
-    .post(`${USER_SERVER}/api/login`, dataToSubmit)
+    .post(`${USER_SERVER}/auth/login`, dataToSubmit)
     .then((response) => response.data)
     .catch((error) => {});
   return {
-    type: LOGIN_SUCCESS,
+    type: LOGIN,
     payload: request,
   };
 };
 
-const initialState = {
-  isAuth: false,
+export const signup = (dataToSubmit) => {
+  const request = axios
+    .post(`${USER_SERVER}/auth/signin`, dataToSubmit)
+    .then((response) => response.data)
+    .catch((error) => {});
+  return {
+    type: SIGNUP,
+    payload: request,
+  };
 };
 
-const actions = (state = initialState, action) => {
+export const getTotalTime = (id) => {
+  const request = axios
+    .get(`${USER_SERVER}/main/mytime?user_idx=${id}`)
+    .then((response) => response.data)
+    .catch((error) => {});
+  return { type: GET_TOTAL_TIME, payload: request };
+};
+
+export const getRanking = () => {
+  const request = axios
+    .get(`${USER_SERVER}/main/rank`)
+    .then((response) => response.data)
+    .catch((error) => {});
+  return { type: GET_RANK, payload: request };
+};
+
+const actions = (state = {}, action) => {
   switch (action.type) {
-    case LOGIN_SUCCESS:
+    case LOGIN:
       return { ...state, isAuth: action.payload.success };
     default:
       return state;
