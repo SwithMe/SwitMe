14922c6f73e1d4b910f634e35657fc931ab114bc
yhjune <yhjune09@gmail.com>
2021-07-28 17:12:34 +0900

    testing

--- BackEnd/src/main/java/com/watch/switme/repository/StudyRepository.java
+++ BackEnd/src/main/java/com/watch/switme/repository/StudyRepository.java
@@ -1,7 +1,15 @@
 package com.watch.switme.repository;
 
+<<<<<<< Updated upstream
 import com.watch.switme.dto.Study;
+=======
+import com.watch.switme.domain.ChatRoom;
+import com.watch.switme.domain.Study;
+>>>>>>> Stashed changes
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface StudyRepository extends JpaRepository<Study, Long> {
+
 }
--- BackEnd/src/main/java/com/watch/switme/service/StudyService.java
+++ BackEnd/src/main/java/com/watch/switme/service/StudyService.java
@@ -1,7 +1,21 @@
 package com.watch.switme.service;
 
+import com.watch.switme.dto.makeStudyDto;
+import com.watch.switme.repository.StudyRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
+
+@RequiredArgsConstructor
 @Service
 public class StudyService {
+    private final StudyRepository studyRepository;
+
+    @Transactional
+    public Integer add(makeStudyDto makeStudyDto){ return studyRepository.save(makeStudyDto.toEntity()).getStudy_idx();}
+
+
+
 }
