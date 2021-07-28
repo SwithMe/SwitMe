cfd2957cae15ce2cf6be1afbb10541b9abc7a23f
jungeun <svaha@ewhain.net>
2021-07-28 11:23:09 +0900

    수정

---
+++ BackEnd/src/main/java/com/jwtappliedexample/example/security/exception/InputNotFoundException.java
@@ -0,0 +1,9 @@
+package com.jwtappliedexample.example.security.exception;
+
+public class InputNotFoundException extends RuntimeException {
+
+    public InputNotFoundException(){
+        super();
+    }
+
+}
