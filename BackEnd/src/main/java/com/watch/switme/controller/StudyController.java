0665178d23f528105534d41bb6ccb14435daaa09
jungeun <svaha@ewhain.net>
2021-07-29 14:46:36 +0900

    study 기능구현(조회/등록)

--- BackEnd/src/main/java/com/watch/switme/controller/StudyController.java
+++ BackEnd/src/main/java/com/watch/switme/controller/StudyController.java
@@ -1,10 +1,14 @@
 package com.watch.switme.controller;
 
+import com.watch.switme.domain.Study;
 import com.watch.switme.dto.makeStudyDto;
 import com.watch.switme.repository.StudyRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @RestController
@@ -14,9 +18,23 @@ public class StudyController {
     @Autowired
     StudyRepository studyRepository;
 
+    @Autowired
+    public StudyController(StudyRepository studyRepository){
+        this.studyRepository=studyRepository;
+    }
     //온/오프라인 스터디 개설하기
 
-
+    @PostMapping("/array/enroll")
+    public void createnewStudy(@RequestBody Study study){
+        // 제약조건을 통과못하면 error 리턴한다.
+        // 여기 채워야함!
+            studyRepository.save(study);
+    }
+    //전체 스터디 리스트 가져오기.
+    @GetMapping("/alllist")
+    public Iterable<Study> list(){
+        return studyRepository.findAll();
+    }
     //스터디 수정하기
     @PostMapping("/array/fix/{study_idx}")
     public void fixStudyDetail(@PathVariable int study_idx){
@@ -24,9 +42,10 @@ public class StudyController {
     }
 
     //스터디 세부사항 보여주기
-    @GetMapping("/array/{study_idx}")
-    public void showStudyDetail(@PathVariable int study_idx){
-
+    @PostMapping("/array/{title}")
+    public Iterable<Study> showStudyDetail(@PathVariable String title){
+        System.out.println(title);
+        return studyRepository.findByTitle(title);
     }
 
     //스터디 탈퇴하기
@@ -40,7 +59,4 @@ public class StudyController {
     public void JoinStudy(@PathVariable int user_udx, @PathVariable int study_idx){
 
     }
-
-
-
 }
--- BackEnd/src/main/java/com/watch/switme/service/StudyService.java
+++ BackEnd/src/main/java/com/watch/switme/service/StudyService.java
@@ -3,15 +3,28 @@ package com.watch.switme.service;
 import com.watch.switme.domain.Study;
 import com.watch.switme.repository.StudyRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class StudyService {
+
+    @Autowired
     private final StudyRepository studyRepository;
 
+
     public Study findByStudyIdx(Long study_idx){
-        return studyRepository.findFirstByStudyIdx(study_idx);
+        return (Study) studyRepository.findFirstByStudyIdx(study_idx);
     }
+
+    //스터디 추가..?
+
+    public void createStudy(Study study){
+        studyRepository.save(study);
+    }
+
 
 }
