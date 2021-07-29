a944b4e349e9edf3521028aa61884cc511bdd7bd
hyojin530 <dkakfl56@gmail.com>
2021-07-29 17:41:21 +0900

    mypage userInfo, studyList, timerLog

---
+++ BackEnd/src/main/java/com/watch/switme/repository/UserStudyRepository.java
@@ -0,0 +1,15 @@
+package com.watch.switme.repository;
+
+import com.watch.switme.domain.UserStudy;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserStudyRepository extends JpaRepository<UserStudy, Long> {
+
+    @Query(value = "select * from User_study u where u.user_idx = ?1", nativeQuery = true)
+    List<UserStudy> findByUserIdx(Long user_idx);
+}
--- BackEnd/src/main/java/com/watch/switme/service/MyPageService.java
+++ BackEnd/src/main/java/com/watch/switme/service/MyPageService.java
@@ -1,17 +1,45 @@
 package com.watch.switme.service;
 
-import com.watch.switme.repository.StudyRepository;
-import com.watch.switme.repository.TimerRepository;
-import com.watch.switme.repository.UserDataExtraRepository;
-import com.watch.switme.repository.UserRepository;
+import com.watch.switme.domain.UserStudy;
+import com.watch.switme.dto.UserStudyListResponseDto;
+import com.watch.switme.dto.UserTimerLogResponseDto;
+import com.watch.switme.repository.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class MyPageService {
     private final UserRepository userRepository;
     private final UserDataExtraRepository userDataExtraRepository;
-    private final StudyRepository studyRepository;
-    private final TimerRepository timerRepository;
+    private final UserStudyRepository userStudyRepository;
+    private final TimerLogRepository timerLogRepository;
+
+    public List<UserStudyListResponseDto> getUserStudyList(Long user_idx){
+        List<UserStudy> userStudyList = userStudyRepository.findByUserIdx(user_idx);
+        List<UserStudyListResponseDto> userStudyListResponseDtoList = new ArrayList<>();
+
+        for(UserStudy study: userStudyList){
+            UserStudyListResponseDto userStudyListResponseDto = UserStudyListResponseDto.builder()
+                    .study_idx(study.getStudy().getStudy_idx())
+                    .study_title(study.getStudy().getTitle())
+                    .study_image(study.getStudy().getImage())
+                    .start_date(LocalDateTime.now()) //study.getStudy().getTermstart()
+                    .end_date(LocalDateTime.now()) //study.getStudy().getTermend()
+                    .warning(study.getWarning())
+                    .build();
+
+            userStudyListResponseDtoList.add(userStudyListResponseDto);
+        }
+
+        return userStudyListResponseDtoList;
+    }
+
+    public List<UserTimerLogResponseDto> getUserTimerLog(Long user_idx){
+        return timerLogRepository.findTop20ByUserIdx(user_idx);
+    }
 }
