2bc290d06a38c0e147b5c9c5dcb1bb4fbf509658
yhjune <yhjune09@gmail.com>
2021-08-01 06:37:01 +0900

    오류?

--- BackEnd/src/main/java/com/watch/switme/controller/StudyController.java
+++ BackEnd/src/main/java/com/watch/switme/controller/StudyController.java
@@ -10,15 +10,17 @@ import com.watch.switme.service.UserStudyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import javax.sound.midi.SysexMessage;
 import javax.swing.plaf.synth.SynthTextAreaUI;
+
 import java.util.List;
 
 /*
 * 2. 스터디 가입하기 채우기
 * 4. 스터디 수정하기
 * */
+
+
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/list")
@@ -26,27 +28,25 @@ public class StudyController {
 
     @Autowired
     UserStudyService userStudyService;
-
     @Autowired
     StudyRepository studyRepository;
-
     @Autowired
     UserRepository userRepository;
-
     UserStudyRepository userStudyRepository;
 
     @Autowired
-    public StudyController(StudyRepository studyRepository){
-        this.studyRepository=studyRepository;
+    public StudyController(StudyRepository studyRepository) {
+        this.studyRepository = studyRepository;
     }
 
     //온/오프라인 스터디 개설하기
+    // 3. 사진 넣는 거 바꾸기 resources save /server 주소 /폴더 위치
     //makestudydto 를 받는다 (고 생각)
     @PostMapping("/array/enroll")
-    public Study createnewStudy(@RequestBody MakeStudyDto makeStudyDto){
-        System.out.println("\n\n안"+makeStudyDto.getLeader());
-        User user=userRepository.findFirstByUserIdx(makeStudyDto.getLeader());
-        int temp=user.getManner_temperature();
+    public Study createnewStudy(@RequestBody MakeStudyDto makeStudyDto) {
+        System.out.println("\n\n안" + makeStudyDto.getLeader());
+        User user = userRepository.findFirstByUserIdx(makeStudyDto.getLeader());
+        int temp = user.getManner_temperature();
         System.out.println(user.getManner_temperature());
         // study main 에 저장하면 됨.
         Study study = Study.builder()
@@ -67,15 +67,12 @@ public class StudyController {
                 .avgMannerTemperature(temp)
                 .build();
         return studyRepository.save(study);
-        /*Optional<Study> exist=studyRepository.findByTitle(study.getTitle());
-        if(exist.isPresent()) {
-            throw new Exception(HttpStatus.CONFLICT, "오류");
-        }*/
-        // 1. leader 값에 만드는 사람의 user_idx 넣기
-        // 2. leader name 추가하기
-        // 3. 사진 넣는 거 바꾸기 resources save /server 주소 /폴더 위치
+        }
 
-    }
+    //스터디 수정하기
+    //@PostMapping("/array/fix/{study_idx}")
+    //public Iterable<Study> edit(@PathVariable Long study_idx, @RequestBody Study study) {
+    //   }
 
     //전체 스터디 리스트 가져오기.
     @GetMapping("/alllist")
@@ -83,52 +80,40 @@ public class StudyController {
         return studyRepository.findAll();
     }
 
-    //스터디 수정하기
-  //  @PostMapping("/array/fix/{study_idx}")
-   // public Iterable<Study> edit(@PathVariable Long study_idx, @RequestBody Study study){
-    //}
-
-    //스터디 가입하기(테스트 필요함)
-    /*@PostMapping("/array/join/{user_idx}/{study_idx}")
-    public JoinStudyDto JoinStudy(@PathVariable Long user_idx, @PathVariable Long study_idx){
-        JoinStudyDto joinStudyDto = JoinStudyDto.builder()
-                .amLeader(UserYesOrNo.N)
-                .study_idx(study_idx)
-                .user_idx(user_idx)
-                .warning(0)
-                .joinDate(LocalDateTime.now())
-                .activate(UserYesOrNo.Y)
-                .build();
-        return joinStudyDto;//studyRepository.save(joinStudyDto.toEntity()).getUserStudyIdx();
-    }*/
-
+    //스터디 가입하기
     @PostMapping("/array/join/{user_idx}/{study_idx}")
-    public Long JoinStudy(@PathVariable Long user_idx, @PathVariable Long study_idx){
-        return userStudyService.join(user_idx,study_idx);
-    }
+    public Long JoinStudy (@PathVariable Long user_idx, @PathVariable Long study_idx){
+        return userStudyService.join(user_idx, study_idx);
+        }
 
     //스터디 탈퇴하기
-     @DeleteMapping("/array/leave/{user_study_idx}")
-        public void LeaveStudy(@PathVariable("user_study_idx") long user_study_idx){
-            userStudyService.leave(user_study_idx);}
+    @DeleteMapping("/array/leave/{user_study_idx}")
+    public void LeaveStudy ( @PathVariable("user_study_idx") long user_study_idx){
+        userStudyService.leave(user_study_idx);}
 
-    //스터디 세부사항 보여주기
-    @GetMapping("/array/study/{study_idx}")
-    public Study showStudyDetail(@PathVariable Long study_idx){
-        return studyRepository.findByStudy_idx(study_idx);
+        //스터디 가입여부 확인하기
+        //@GetMapping("/array/status/{user_idx}/{study_idx}")
+        //public void statuStudy(){
+        // }
+
+        //스터디 세부사항 보여주기
+        @GetMapping("/array/study/{study_idx}")
+        public Study showStudyDetail (@PathVariable Long study_idx){
+            return studyRepository.findByStudy_idx(study_idx);
+        }
+
+        //스터디 검색 기능
+        @PostMapping("/array")
+        public List<Study> example (@RequestBody SearchStudyDto searchStudyDto){
+            return studyRepository.getQuery
+                    (
+                            searchStudyDto.getLeader(),
+                            searchStudyDto.getTitle(),
+                            searchStudyDto.getSize(),
+                            searchStudyDto.getType(),
+                            searchStudyDto.getActivate()
+                    );
+        }
+
     }
 
-    //스터디 검색 기능
-    @PostMapping("/array")
-    public List<Study> example(@RequestBody SearchStudyDto searchStudyDto){
-        return studyRepository.getQuery
-                (
-                        searchStudyDto.getLeader(),
-                        searchStudyDto.getTitle(),
-                        searchStudyDto.getSize(),
-                        searchStudyDto.getType(),
-                        searchStudyDto.getActivate()
-                );
-    }
-
-}
